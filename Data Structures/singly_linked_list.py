d39559aa942baa5ad5e983817a51e28a189bf3ed
Carlos Trejo <guadarramac74@gmail.com>
2020-01-30 14:16:55 -0600

    Fixed Anti-Pattern and Bug Risk PYL-W0120 PYL-R1720 PTC-W0019

--- Data Structures/singly_linked_list.py
+++ Data Structures/singly_linked_list.py
@@ -8,24 +8,25 @@ class LinkedList:
         self.head = None
         self.tail = None
         self.node_ids = [] # keeps track of the nodes that are in the linked list, helpful when adding a node relative to another node, also helpful for determining length
-    
+
     def __str__(self) -> str:
         """Returns the content of the linked list."""
         current_node = self.head
-        data = tuple() # Using tuple because it uses less memory
+        data = () # Using tuple because it uses less memory
         while current_node:
             data += (current_node.data,)
             current_node = current_node.next
-        else:
-            return str(data)
-    
+
+        return str(data)
+
     def __repr__(self):
         return str(self)
-    
+
     def __getitem__(self, key: int) -> Node:
         if (key is not int) or (key < 0) or (key >= len(self.node_ids)) or (len(self.node_ids) == 0):
             raise IndexError("linked list index out of range")
-        elif key == len(self.node_ids) - 1: return self.tail
+
+        if key == len(self.node_ids) - 1: return self.tail
         elif key == 0: return self.head
         else:
             node = self.head.next
@@ -33,9 +34,9 @@ class LinkedList:
             while index != key:
                 node = node.next
                 index += 1
-            else: # On index and key match, return node
-                return node
-    
+
+            return node # On index and key match, return node
+
     def append(self, data) -> None:
         """Adds (appends) a node to the end of the list."""
         new_node = Node(data)
@@ -44,29 +45,31 @@ class LinkedList:
             self.tail = new_node
         else:
             self.tail.next, self.tail = new_node, new_node
-        
+
         self.node_ids.append(id(new_node))
 
     def prepend(self, data) -> None:
         """Adds a node to the beginning of the list (prepend)."""
         self.head = Node(data, next_node=self.head)
         self.node_ids.append(id(self.head))
-    
+
     def append_at_node(self, node: Node, data):
         """Inserts a new node after the given node."""
         if not isinstance(node, Node):
             raise TypeError("Argument 1 is not a node.")
-        elif id(node) not in self.node_ids:
+
+        if id(node) not in self.node_ids:
             raise IndexError("Node not in linked list.")
-        else:
-            node.next = Node(data, next_node=node.next)
-            self.node_ids.append(id(node.next))
+
+        node.next = Node(data, next_node=node.next)
+        self.node_ids.append(id(node.next))
 
     def insert_at_index(self, index: int, data) -> None:
         """Inserts a node at the given index, if the index is out of range, then it appends it to the end."""
         if type(index) is not int:
             raise TypeError("Index must be an integer.")
-        elif index == 0:
+
+        if index == 0:
             self.prepend(data)
         elif index >= len(self.node_ids):
             self.append(data)
@@ -79,10 +82,11 @@ class LinkedList:
                 last_node = node
                 node = node.next
                 node_index += 1
-            else: # Node indexes matched, so insert node
-                last_node.next = Node(data,next_node=node)
-                self.node_ids.append(id(last_node.next))
-            
+
+            # Node indexes matched, so insert node
+            last_node.next = Node(data,next_node=node)
+            self.node_ids.append(id(last_node.next))
+
     def remove_node(self, data) -> None:
         """Removes a node that matches the given data."""
         node = self.head
@@ -93,4 +97,3 @@ class LinkedList:
             raise ValueError("Data not in linked list")
 
         # node found
-        
--- Data Structures/stack.py
+++ Data Structures/stack.py
@@ -2,23 +2,22 @@ class Stack:
     def __init__(self):
         self.size = 0
         self.items = []
-    
+
     def peek(self) -> object:
         """Returns the top element of the stack without deleting it."""
         return self.items[-1] if self.items else None
-    
+
     def push(self, item) -> None:
         """Adds an element to the top of the stack."""
         self.items.append(item)
         self.size += 1
-    
+
     def pop(self) -> object:
         """Returns and removes the top element of the stack."""
-        if self.is_empty():
-            raise IndexError("Stack is empty")
-        else:
-            self.size -= 1
-            return self.items.pop()
+        if self.is_empty(): raise IndexError("Stack is empty")
+
+        self.size -= 1
+        return self.items.pop()
 
     def get_stack(self) -> list:
         """Returns all elements of the stack without clearing the stack."""
@@ -27,11 +26,11 @@ class Stack:
     def is_empty(self) -> bool:
         """Returns True if the stack is empty, False otherwise."""
         return not self.items
-    
+
     def clear(self) -> None:
         """Empties the stack."""
         self.items.clear()
         self.size = 0
-    
+
     def __len__(self) -> int:
         return len(self.items)
