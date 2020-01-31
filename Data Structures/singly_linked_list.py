e2c8ff07cb4e6b387afc92475e8f94306f497739
Carlos Trejo <guadarramac74@gmail.com>
2020-01-31 13:51:17 -0600

    Added new functionality to LinkedList and wrote new tests

--- Data Structures/singly_linked_list.py
+++ Data Structures/singly_linked_list.py
@@ -8,7 +8,10 @@ class LinkedList:
         self.head = None
         self.tail = None
         self.node_ids = [] # keeps track of the nodes that are in the linked list, helpful when adding a node relative to another node, also helpful for determining length
-
+    
+    def __len__(self):
+        return len(self.node_ids)
+    
     def __str__(self) -> str:
         """Returns the content of the linked list."""
         current_node = self.head
@@ -23,20 +26,32 @@ class LinkedList:
         return str(self)
 
     def __getitem__(self, key: int) -> Node:
-        if (key is not int) or (key < 0) or (key >= len(self.node_ids)) or (len(self.node_ids) == 0):
-            raise IndexError("linked list index out of range")
+        """Returns nodes on given key (index), does not support negative indexing smaller than -1 or slices."""
+        if (type(key) is not int): raise TypeError("indices must be integers not str.")
+        if (type(key) is slice): raise NotImplementedError("Slices have are not supported")
 
-        if key == len(self.node_ids) - 1: return self.tail
-        elif key == 0: return self.head
-        else:
-            node = self.head.next
-            index = 1
+        pre_tests = (
+            (key >= len(self.node_ids)), # index out of max range
+            (key < 0-len(self.node_ids)), # index out of min range
+            (len(self.node_ids) == 0) # linked list is empty
+        )
+        
+        if any(pre_tests): raise IndexError("linked list index out of range")
+
+        if key in (0, 0 - len(self.node_ids)): return self.head
+        elif key in (-1, len(self.node_ids) - 1): return self.tail
+        else: # if index is negative...
+            node = self.head.next # Head was returned in first if statement, no need to start at head
+            index = 1-len(self.node_ids) if key < 0 else 1 # Because head was returned, there is also no need to have index start at 0 or the min negative index
             while index != key:
                 node = node.next
                 index += 1
 
             return node # On index and key match, return node
 
+    def __setitem__(self, key, value):
+        raise NotImplementedError(f"llist[{key}] = {value} not implemented... yet.")
+    
     def append(self, data) -> None:
         """Adds (appends) a node to the end of the list."""
         new_node = Node(data)
@@ -59,16 +74,16 @@ class LinkedList:
             raise TypeError("Argument 1 is not a node.")
 
         if id(node) not in self.node_ids:
-            raise IndexError("Node not in linked list.")
+            raise KeyError("Node not in linked list.")
 
         node.next = Node(data, next_node=node.next)
         self.node_ids.append(id(node.next))
 
     def insert_at_index(self, index: int, data) -> None:
         """Inserts a node at the given index, if the index is out of range, then it appends it to the end."""
-        if type(index) is not int:
-            raise TypeError("Index must be an integer.")
-
+        if type(index) is not int: raise TypeError("index must be an integer.")
+        if index < 0: raise ValueError("index must be non-negative")
+        
         if index == 0:
             self.prepend(data)
         elif index >= len(self.node_ids):
@@ -94,6 +109,6 @@ class LinkedList:
             if node.data == data: break # break if node data matches given data
             else: node = node.next
         else: # node not found
-            raise ValueError("Data not in linked list")
+            raise ValueError(f"Data {data} not in linked list")
 
         # node found
--- Data Structures/test_singly_linked_list.py
+++ Data Structures/test_singly_linked_list.py
@@ -1,3 +1,132 @@
-import singly_linked_list
-import sentry_sdk
-sentry_sdk.init("https://f8c2cb0bb7714decbfd8b6f1be691d30@sentry.io/2083487")
+import pytest
+
+def setup_function(function):
+    global llist
+    llist = singly_linked_list.LinkedList()
+
+def test_append_len():
+    """Doing llist.append(obj) should add obj at the end of the linked list."""
+    assert len(llist) == 0
+    objects = ('a', 1, object(), ['list'], ('tuple',), {0: 'dict'}, {'set'})
+    for length,o in enumerate(objects, start=1):
+        llist.append(o)
+        assert len(llist) == length
+        assert llist.tail == o
+
+def test_str_repr():
+    """Calling str() and repr() on a linked list should output the data in a tuple as a string."""
+    assert str(llist) == '()'
+    assert repr(llist) == '()'
+    llist.append('a')
+    llist.append(123)
+    assert str(llist) == "('a', 123)"
+    assert repr(llist) == "('a', 123)"
+
+def test_getitem():
+    """Given an integer, n, llist[n] should return the node at position n"""
+    with pytest.raises(TypeError):
+        llist['a']
+    
+    with pytest.raises(IndexError):
+        llist[0]
+    
+    with pytest.raises(NotImplementedError):
+        llist[:]
+    
+    for c in 'abcdefg': llist.append(c)
+    
+    assert llist[0] == 'a'
+    assert llist[2] == 'c'
+
+    assert llist[-1] == 'g'
+    assert llist[-3] == 'e'
+
+def test_setitem():
+    """llist[n] == obj should insert obj at index n"""
+    with pytest.raises(IndexError):
+        llist[0] = 'new data'
+    
+    llist.append('a')
+    
+    with pytest.raises(TypeError):
+        llist['a'] = 'new data'
+    
+    with pytest.raises(TypeError):
+        llist[2:3] = 'new data'
+    
+    llist[0] = 'new data'
+
+    assert len(llist) == 1
+    assert llist[0].data == 'new data'
+
+    for c in 'abc': llist.append(x)
+
+    llist[1] = 'x'
+
+    assert len(llist) == 4
+    assert str(llist) == "('new data', 'x', 'b', 'c')"
+
+def test_prepend():
+    """Doing llist.prepend(obj) should add a new node with data, obj, at the beginning of the linked list."""
+    objects = ('a', 1, object(), ['list'], ('tuple',), {0: 'dict'}, {'set'})
+    for length,o in enumerate(objects, start=1k):
+        llist.prepend(o)
+        assert len(llist) == length
+        assert llist.head == o
+
+def test_append_at_node():
+    """Doing llist.append_at_node(node, obj), should add a new node containing data, obj, after the given node."""
+    for o in (1, 'a', [], {}, ()):
+        with pytest.raises(TypeError):
+            llist.append_at_node(o, 'DATA')
+    
+    outside_node = singly_linked_list.Node('new node data')    
+    with pytest.raises(KeyError):
+        llist.append_at_node(outside_node, 'New-er node data')
+    
+    for c in 'abc':
+        llist.append(c)
+    
+    llist.append_at_node(llist[1], 'X')
+    assert str(llist) == "('a', 'b', 'X', 'c')"
+
+def test_insert_at_index():
+    """Doing llist.insert_at_index(n) should insert a new node at the given index, n."""
+    for c in 'abc': llist.append(c)
+    
+    with pytest.raises(TypeError):
+        llist.insert_at_index('index', 'new data')
+    
+    with pytest.raises(ValueError):
+        llist.insert_at_index(-2, 'new data')
+    
+    llist.insert_at_index(1, 'X')
+    assert str(llist) == "('a', 'X', 'b', 'c')"
+
+    llist.insert_at_index(400, 'tail')
+    assert str(llist) == "('a', 'X', 'b', 'c', 'tail')"
+    
+    llist.insert_at_index(0, 'head')
+    assert str(llist) == "('head', 'a', 'X', 'b', 'c', 'tail')"
+    assert len(llist) == 6
+
+def test_remove_node():
+    """Doing llist.remove_node(data) should remove the first occurence of the node containing the given data."""
+    with pytest.raises(ValueError):
+        llist.remove_node('a')
+
+    for c in 'abccd': llist.append(c)
+
+    node_3 = llist[2]
+    llist.remove_node('c')
+    assert llist[2] != node_3
+    assert len(llist) == 4
+    assert str(llist) == "('a', 'b', 'c', 'd')"
+    
+    with pytest.raises(ValueError):
+        llist.remove_node('data')
+
+
+def teardown_function(function):
+    global llist
+    del llist
