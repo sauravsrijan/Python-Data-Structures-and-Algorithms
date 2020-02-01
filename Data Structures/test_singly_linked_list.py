c915dbf160eb2f09724ce98c9ae62b172a004559
Carlos Trejo <guadarramac74@gmail.com>
2020-02-01 00:11:42 -0600

    Fixed .travis pytest error and typo in SLList test

--- Data Structures/test_singly_linked_list.py
+++ Data Structures/test_singly_linked_list.py
@@ -118,7 +118,7 @@ def test_insert_at_index():
     llist.insert_at_index(0, 'head')
     assert str(llist) == "('head', 'a', 'X', 'b', 'c', 'tail')"
 
-    llist.test_insert_at_index(3, '3')
+    llist.insert_at_index(3, '3')
     assert str(llist) == "('head', 'a', 'X', '3', 'b', 'c', 'tail')"
     assert len(llist) == 6
 
